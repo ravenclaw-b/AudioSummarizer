4066022bcf9ec3e8f1dd56a915423090b417cf35
Bamdad Khoshkhah <bamdad.kho@gmail.com>
2024-05-19 19:48:01 +0300

    functional v1

--- main.py
+++ main.py
@@ -1,46 +1,38 @@
 
-# %%
 from openai import OpenAI
 import dotenv
 import os
 import whisper
-# %%
+
+# Define the path for the input file
+path = input("audio file path: ")
 
 #load transcribe the file
 model = whisper.load_model("base")
-transcript = model.transcribe("pyCut.mp3")
+transcript = model.transcribe(path)
+print("file transcribed")
 
-# %%
 # Load the API key from environment variables
 dotenv.load_dotenv()
 KEY = os.getenv("KEY")
-# %%
 
 # Create an instance of the OpenAI client
 client = OpenAI(api_key=KEY)
 
-# Define the path for the output file
-path = "pyCut.mp3"
-
 # Write the transcript to a file
 with open("transcript.txt", "w+") as f:
    f.write(transcript["text"])
-# %%
-   
+
 # Define a function to summarize the transcript
 def summarize(transcript):
     response = client.chat.completions.create(
-        model="gpt-4-turbo-preview",
+        model="gpt-3.5-turbo-0125",
         temperature=0,
         messages=[
             {
                 "role": "system",
-                "content": """You are a highly skilled AI trained in language comprehension and 
-                summarization. I would like you to read the following text and summarize it into a 
-                concise abstract paragraph. Aim to retain the most important points, providing a 
-                coherent and readable summary that could help a person understand the main points 
-                of the discussion without needing to read the entire text. Please avoid unnecessary 
-                details or tangential points."""
+                "content": """Your name is AUSUM. You are an audio summerizing program. You will take a transcrpt of a lesson and return a highly detailed summery of what happened. Keep in mind that the transcript is made automatically and may be incorrect,
+                so please disregard nonesense in the transcript. You are designed to be used in school, and may make flashcard, quizzes, and tests based on the material given. You may answer questions based on the material given. """
             },
             {
                 "role": "user",
@@ -51,5 +43,6 @@ def summarize(transcript):
     return response.choices[0].message.content
 
 # Write the summarized text to a file
-with open("summary2.txt", "w") as f:
+with open("summary.txt", "w") as f:
     f.write(summarize(transcript["text"]))
+print("summery outputted in summery.txt")
---
+++ test.py
@@ -0,0 +1,4 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
